12c7afc05bc258dc488903361c35bf16c04bfaf5
Yasushi Sakai <yasushi.accounts@fastmail.com>
2025-07-05 13:54:04 -0400

    Neovim Settings

--- flake.nix
+++ flake.nix
@@ -11,254 +11,311 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
-  let
-    ###########################################################
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+      home-manager,
+    }:
+    let
+      ###########################################################
 
-    username = "yasushi";
-    hostname = "sunny"; # = `scutil --get LocalHostName`
+      username = "yasushi";
+      hostname = "sunny"; # = `scutil --get LocalHostName`
 
-    # git 
-    fullname = "Yasushi Sakai";
-    email = "yasushi.accounts@fastmail.com";
+      # git
+      fullname = "Yasushi Sakai";
+      email = "yasushi.accounts@fastmail.com";
 
-    ###########################################################
+      # zk
+      zk_directory = "$HOME/Documents/memo/";
 
-    configuration = { pkgs, ... }: {
+      ###########################################################
 
-      # NOTE: List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      nix.settings.experimental-features = "nix-command flakes";
+      configuration =
+        { pkgs, ... }:
+        {
 
-      nix.gc = {
-        automatic = true;
-	# dates = "weekly"; # if not nix-darwin
-        interval = { Weekday = 0; Hour = 0; Minute = 0; };
-        options = "--delete-older-than 300d";
-      };
+          # NOTE: List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          nix.settings.experimental-features = "nix-command flakes";
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      system.stateVersion = 6; # don't change this!
-      system.primaryUser = username;
-      
-      system.keyboard = {
-        enableKeyMapping = true;
-        remapCapsLockToControl = true;
-      };
-
-      system.activationScripts.postUserActivateion.text = ''
-        sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
-      '';
-
-      system.defaults = {
-
-      	dock = {
-	  show-recents = false;
-	  autohide = true;
-	  mru-spaces = false;
-	  persistent-apps = [
-	    { app = "/System/Applications/Messages.app"; }
-            { app = "/System/Applications/System Settings.app"; }
-	  ];
-	};
-
-	screencapture.location = "/Users/${username}/Screenshots";
-
-	finder.FXPreferredViewStyle = "Nlsv"; # defaults to list view on finder
-	finder.NewWindowTarget = "Home";
-	finder.ShowPathbar = true;
-	finder.ShowStatusBar = true;
-
-	NSGlobalDomain = {
-          AppleShowAllExtensions = true;
-          AppleMeasurementUnits = "Centimeters";
-          AppleICUForce24HourTime = true;
-	};
-      };
-
-      # Touch ID
-      security.pam.services.sudo_local.touchIdAuth = true;
-      
-      # Microsoft Office was installed through MIT credentials
-      # https://m365.cloud.microsoft/chat/?auth=2
-
-      nixpkgs.hostPlatform = "aarch64-darwin";
-      nixpkgs.config.allowUnfree = true;
-
-      environment.systemPackages = with pkgs; [ 
-        ffmpeg 
-        pandoc
-	imagemagick
-	tectonic
-        ghostscript
-      ];
-
-      programs.zsh = {
-        enable = true;
-        enableFastSyntaxHighlighting = true;
-        enableFzfCompletion = true;
-        enableFzfHistory = true;
-      };
-
-      fonts = {
-      	packages = with pkgs; [
-        hackgen-nf-font 
-	cm_unicode
-        ];
-      };
-      
-      homebrew = {
-      	enable = true;
-	onActivation.cleanup= "zap";
-
-	brews = [];
-	casks = [ 
-          "ghostty" 
-	  "bitwarden"
-          "figma"
-          "slack"
-          "discord"
-          "hammerspoon"
-	  "zoom" # moved from systemPackages, for the alias
-	  "whatsapp"
-	  "google-chrome" # fall back browser
-	  "little-snitch"
-	  "obs"
-	  "loopback"
-	  "font-sf-mono"
-	  "font-sf-pro"
-	  "font-new-york"
-        ];
-
-	# NOTE: this will not get purged through `onActivation.cleanup`
-	masApps = {
-	  # apple products
-	  "Final Cut Pro" = 424389933;
-	  "Logic Pro" = 634148309;
-	  "Compressor" = 424390742;
-	  "Keynote" = 409183694;
-	  "Xcode" = 497799835;
-	  "Tailscale" = 1475387142; # Tailscale said mas rather than brew
-	};
-      };
-
-      users.users.${username}.home = "/Users/${username}";
-
-      home-manager = {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        users.${username} = { pkgs, ... }: {
-          home.stateVersion = "24.11"; # don't change this!
-
-	  home.packages = with pkgs; [
-	    fzf
-    	    claude-code
-	  ];
-
-          programs.neovim = {
-            enable = true;
-            defaultEditor = true;
-	    vimAlias = true;
-	    vimdiffAlias = true;
-	    viAlias = true;
-          };
-
-	  programs.zsh = {
-            enable = true;
-            shellAliases = {
-              rebuild = "sudo darwin-rebuild switch --flake ~/.config/nix";
-	      update = "nix flake update --flake ~/.config/nix";
-	      cleanup = "sudo nix-env --delete-generations +5";
-	      memo = "zk edit -i";
-	      config = "nvim ~/.config/nix/flake.nix";
-	      config-nvim = "nvim ~/.config/nix/nvim/init.lua";
+          nix.gc = {
+            automatic = true;
+            # dates = "weekly"; # if not nix-darwin
+            interval = {
+              Weekday = 0;
+              Hour = 0;
+              Minute = 0;
             };
-          };
-          
-          programs.gh = {
-            enable = true;
-            extensions = with pkgs; [
-              gh-copilot
-            ];
-            settings = {
-              git_protocol = "https";
-              prompt = "enabled";
-            };
+            options = "--delete-older-than 300d";
           };
 
-          programs.git = {
-            enable = true;
-            userName = fullname;
-            userEmail = email;
-            extraConfig = {
-              init.defaultBranch = "main";
-              pull.rebase = true;
-            };
-            ignores = [ ".DS_Store" ];
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          system.stateVersion = 6; # don't change this!
+          system.primaryUser = username;
+
+          system.keyboard = {
+            enableKeyMapping = true;
+            remapCapsLockToControl = true;
           };
 
-	  programs.zoxide = {
-            enable = true;
-            enableZshIntegration = true;
-            options = [ "--cmd cd" ];
-          };
-
-          # `zk init ~/Documents/memo`
-	  programs.zk = {
-            enable = true;
-	    settings = {
-	      notebook.dir = "~/Documents/memo";
-	      note = {
-	        language = "en";
-		filename = "{{slug title}}-{{id}}";
-		extension = "md";
-		id-charset = "alphanum";
-		id-length = 4;
-		id-case = "lower";
-              };
-	      extra.author = fullname;
-              format.markdown = {
-	        hashtags = true;
-	        colon-tags = true;
-	      };
-	      tool = {
-	      	editor = "nvim";
-		pandoc.extra-args = [ "--bibliography=~/Documents/memo/main.bib" "--citeproc" ];
-	      };
-	    };
-	  };
-
-          # config neovim is left to neovim...
-          home.file.".hammerspoon/init.lua".source = ./hammerspoon/init.lua;
-	  
-          home.file.".config/nvim/init.lua".source = ./nvim/init.lua; 
-
-	  # NOTE: you will need to back up config.local file too 
-	  home.file.".ssh/config".text = ''
-	  Include ~/.ssh/config.local
-	  '';
-
-          home.file.".config/ghostty/config".text = ''
-            font-family = HackGen35 Console NF
-            font-size = 14
+          system.activationScripts.postUserActivateion.text = ''
+            sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
           '';
 
-        };
-      }; # home-manager
+          system.defaults = {
 
+            dock = {
+              show-recents = false;
+              autohide = true;
+              mru-spaces = false;
+              persistent-apps = [
+                { app = "/System/Applications/Messages.app"; }
+                { app = "/System/Applications/System Settings.app"; }
+              ];
+            };
+
+            screencapture.location = "/Users/${username}/Screenshots";
+
+            finder.FXPreferredViewStyle = "Nlsv"; # defaults to list view on finder
+            finder.NewWindowTarget = "Home";
+            finder.ShowPathbar = true;
+            finder.ShowStatusBar = true;
+
+            NSGlobalDomain = {
+              AppleShowAllExtensions = true;
+              AppleMeasurementUnits = "Centimeters";
+              AppleICUForce24HourTime = true;
+              NSAutomaticSpellingCorrectionEnabled = false;
+            };
+          };
+
+          # Touch ID
+          security.pam.services.sudo_local.touchIdAuth = true;
+          security.pam.services.sudo_local.watchIdAuth = true;
+
+          # Microsoft Office was installed through MIT credentials
+          # https://m365.cloud.microsoft/chat/?auth=2
+
+          nixpkgs.hostPlatform = "aarch64-darwin";
+          nixpkgs.config.allowUnfree = true;
+
+          environment.systemPackages = with pkgs; [
+            ffmpeg
+            pandoc
+            imagemagick
+            tectonic
+            ghostscript
+          ];
+
+          programs.zsh = {
+            enable = true;
+            enableFastSyntaxHighlighting = true;
+            enableFzfCompletion = true;
+            enableFzfHistory = true;
+          };
+
+          fonts = {
+            packages = with pkgs; [
+              hackgen-nf-font
+              cm_unicode
+            ];
+          };
+
+          homebrew = {
+            enable = true;
+            onActivation.cleanup = "zap";
+
+            brews = [ "clang-format" ];
+            casks = [
+              "ghostty"
+              "bitwarden"
+              "figma"
+              "slack"
+              "discord"
+              "hammerspoon"
+              "zoom" # moved from systemPackages, for the alias
+              "whatsapp"
+              "google-chrome" # fall back browser
+              "little-snitch"
+              "obs"
+              "loopback"
+              "font-sf-mono"
+              "font-sf-pro"
+              "font-new-york"
+            ];
+
+            # NOTE: this will not get purged through `onActivation.cleanup`
+            masApps = {
+              # apple products
+              "Final Cut Pro" = 424389933;
+              "Logic Pro" = 634148309;
+              "Compressor" = 424390742;
+              "Keynote" = 409183694;
+              "Xcode" = 497799835;
+              "Tailscale" = 1475387142; # Tailscale said mas rather than brew
+            };
+          };
+
+          users.users.${username}.home = "/Users/${username}";
+
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.${username} =
+              { pkgs, ... }:
+              {
+                home.stateVersion = "24.11"; # don't change this!
+
+                home.packages = with pkgs; [
+                  fzf
+                  claude-code
+                  go
+                  tree-sitter
+                  # script language runtimes like python and nodejs
+                  # should be installed through direnv
+
+                  # *** LSPs ***
+                  lua-language-server
+                  gopls
+                  # The following is configured in neovim
+                  # but this should be installed through zk_directorydirenv
+                  # pyright
+                  # typescript-language-server (ts_ls)
+
+                  # *** formatters ***
+                  nixfmt-rfc-style
+                  stylua
+                  # formatters are exceptions to
+                  # python and nodejs
+                  ruff # python
+                  prettierd # js, ts and md
+                ];
+
+                programs.direnv = {
+                  enable = true;
+                  enableZshIntegration = true;
+                  nix-direnv.enable = true;
+                };
+
+                programs.neovim = {
+                  enable = true;
+                  defaultEditor = true;
+                  vimAlias = true;
+                  vimdiffAlias = true;
+                  viAlias = true;
+                };
+
+                programs.zsh = {
+                  enable = true;
+                  autosuggestion.enable = true;
+                  shellAliases = {
+                    rebuild = "sudo darwin-rebuild switch --flake ~/.config/nix";
+                    update = "nix flake update --flake ~/.config/nix";
+                    cleanup = "sudo nix-env --delete-generations +5";
+                    memo = "zk edit -i";
+                    config = "nvim ~/.config/nix/flake.nix";
+                    config-nvim = "nvim ~/.config/nix/nvim/init.lua";
+                    bib = "nvim ${zk_directory}main.bib";
+                  };
+                  initContent = ''
+                    export ZK_NOTEBOOK_DIR="${zk_directory}";
+                  '';
+                };
+
+                programs.gh = {
+                  enable = true;
+                  extensions = with pkgs; [
+                    gh-copilot
+                  ];
+                  settings = {
+                    git_protocol = "https";
+                    prompt = "enabled";
+                  };
+                };
+
+                programs.git = {
+                  enable = true;
+                  userName = fullname;
+                  userEmail = email;
+                  extraConfig = {
+                    init.defaultBranch = "main";
+                    pull.rebase = true;
+                  };
+                  ignores = [ ".DS_Store" ];
+                };
+
+                programs.zoxide = {
+                  enable = true;
+                  enableZshIntegration = true;
+                  options = [ "--cmd cd" ];
+                };
+
+                # `zk init ~/Documents/memo`
+                programs.zk = {
+                  enable = true;
+                  settings = {
+                    notebook.dir = "${zk_directory}";
+                    note = {
+                      language = "en";
+                      filename = "{{slug title}}-{{id}}";
+                      extension = "md";
+                      id-charset = "alphanum";
+                      id-length = 4;
+                      id-case = "lower";
+                    };
+                    extra.author = fullname;
+                    format.markdown = {
+                      hashtags = true;
+                      colon-tags = true;
+                    };
+                    tool = {
+                      editor = "nvim";
+                      pandoc.extra-args = [
+                        "--bibliography=${zk_directory}main.bib"
+                        "--citeproc"
+                      ];
+                    };
+                  };
+                };
+
+                # config neovim is left to neovim...
+                home.file.".hammerspoon/init.lua".source = ./hammerspoon/init.lua;
+
+                home.file.".config/nvim/init.lua".source = ./nvim/init.lua;
+
+                # NOTE: you will need to back up config.local file too
+                home.file.".ssh/config".text = ''
+                  	        Include ~/.ssh/config.local
+                  	        '';
+
+                home.file.".config/ghostty/config".text = ''
+                  font-family = HackGen35 Console NF
+                  font-size = 14
+                '';
+
+                home.file.".config/stylua/stylua.toml".text = ''
+                  indent_type = "Spaces"
+                  indent_width = 4
+                '';
+
+              };
+          }; # home-manager
+        };
+    in
+    {
+      # Build darwin flake using:
+      # $ sudo darwin-rebuild build --flake ~/.config/nix/
+      darwinConfigurations.${hostname} = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          home-manager.darwinModules.home-manager
+        ];
+      };
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ sudo darwin-rebuild build --flake ~/.config/nix/
-    darwinConfigurations.${hostname} = nix-darwin.lib.darwinSystem {
-      modules = [ 
-        configuration
-        home-manager.darwinModules.home-manager
-      ];
-    };
-  };
 }
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,193 @@
+vim.o.number = true
+vim.o.scrolloff = 10
+vim.o.cursorline = true
 
-vim.opt.number = true;
+-- if clipboard is acting weird
+-- vim.schedule(function()
+vim.o.clipboard = "unnamedplus"
+-- end)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- LSP reltated default keymaps `:h grr`
+vim.keymap.set("i", "jk", "<esc>")
+vim.keymap.set("n", "<leader><leader>", ":e #<cr>")
+vim.keymap.set("n", "<leader>bd", "bp | bd #<cr>")
+
+-- auto install lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        error("Error cloning lazy.nvim:\n" .. out)
+    end
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+--
+require("lazy").setup({
+
+    { "NMAC427/guess-indent.nvim", opts = {} },
+
+    {
+        "stevearc/conform.nvim",
+        event = "BufWritePre",
+        cmd = { "ConformInfo" },
+        opts = {
+            notify_on_error = false,
+            formatters_by_ft = {
+                -- make sure you have these in flake.nix
+                lua = { "stylua" },
+                nix = { "nixfmt" },
+                c = { "clang-format" },
+                go = { "gofmt" },
+                python = { "ruff" },
+                swift = { "swift" },
+                markdown = { "prettierd" },
+                javascript = { "prettierd" },
+                typescript = { "prettierd" },
+            },
+            format_on_save = {
+                timeout_ms = 500,
+            },
+        },
+    },
+
+    {
+        "saghen/blink.cmp",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        version = "1.*",
+        opts = {
+            completion = {
+                list = {
+                    selection = { preselect = false },
+                },
+                ghost_text = { enabled = true },
+                documentation = {
+                    auto_show = true,
+                    auto_show_delay_ms = 500,
+                },
+            },
+            keymap = {
+                preset = "enter",
+                ["<C-j>"] = { "select_next", "fallback" },
+                ["<C-k>"] = { "select_prev", "fallback" },
+            },
+            fuzzy = { implementation = "prefer_rust" },
+        },
+    },
+
+    {
+        "neovim/nvim-lspconfig",
+        opts = {},
+        config = function()
+            vim.diagnostic.config({ virtual_lines = { current_line = true } })
+
+            -- sourcekit-lsp also supports c/cpp files
+            -- by default but we let "clangd" do that.
+            -- This is because "clangd" also supports
+            -- other filetypes like, "cuda" and "proto"
+            vim.lsp.config("sourcekit-lsp", {
+                filetypes = { "swift", "objc", "objcpp" },
+            })
+
+            vim.lsp.config("lua_ls", {
+                -- https://luals.github.io/wiki/settings/
+                settings = {
+                    Lua = {
+                        runtime = { version = "LuaJIT" },
+                        diagnostics = {
+                            globals = { "vim", "hs" },
+                        },
+                        workspace = {
+                            library = { vim.env.VIMRUNTIME },
+                            checkThirdParty = false,
+                        },
+                    },
+                },
+            })
+
+            vim.lsp.enable({
+                "lua_ls",
+                "gopls",
+                "clangd",
+                "sourcekit_lsp",
+                -- The following LSPs for python and
+                -- typescript should be configured
+                -- through direnv
+                "pyright",
+                "ts_ls",
+            })
+        end,
+    },
+
+    {
+        "nvim-telescope/telescope.nvim",
+        event = "VimEnter",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make",
+                cond = function()
+                    return vim.fn.executable("make") == 1
+                end,
+            },
+            "nvim-telescope/telescope-bibtex.nvim",
+        },
+        config = function()
+            local ts = require("telescope")
+            ts.setup({
+                extensions = {
+                    bibtex = {
+                        global_files = { "~/Documents/memo/main.bib" },
+                        context = true,
+                        format = "markdown",
+                    },
+                },
+            })
+
+            pcall(ts.load_extension, "fzf")
+            pcall(ts.load_extension, "bibtex")
+
+            local bi = require("telescope.builtin")
+            local map = function(keys, func)
+                vim.keymap.set("n", keys, func)
+            end
+            map("<leader>ff", bi.find_files)
+            map("<leader>fd", bi.grep_string)
+            map("<leader>fg", bi.live_grep)
+            map("<leader>fq", bi.quickfix)
+            map("<leader>fl", bi.loclist)
+            map("<leader>fc", "<cmd>Telescope bibtex<cr>")
+        end,
+    },
+
+    {
+        "nvim-treesitter/nvim-treesitter",
+        branch = "master",
+        lazy = false,
+        build = ":TSUpdate",
+        opts = {
+            -- `tree-sitter` is installed through home-manager
+            auto_install = true,
+            highlight = { enable = true },
+            additional_vim_regex_highlighting = false,
+        },
+    },
+
+    {
+        "zk-org/zk-nvim",
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+        },
+        config = function()
+            require("zk").setup({
+                picker = "telescope",
+            })
+        end,
+    },
+})
